2487c035bf89b43841e7afab72fd824a54f9f794
Corey Adams <corey.adams@yale.edu>
2017-03-13 14:36:45 -0400

    Updating example scripts.

---
+++ UserDev/example/processFile.py
@@ -0,0 +1,45 @@
+import ROOT
+from ROOT import gallery, galleryfmwk, larutil
+
+import sys
+
+
+def process_file(_file):
+
+
+    # Create ana_processor instance
+    my_proc = galleryfmwk.ana_processor()
+
+    # Set input root file
+    # for _f in f:
+        # my_proc.add_input_file(_f)
+
+    my_proc.add_input_file(_file)
+
+
+    # Specify output root file name
+    my_proc.set_ana_output_file(_file.replace('.root', '') + "_ana.root")
+    # my_proc.set_output_file("")
+
+    exampleModule = galleryfmwk.example_ana()
+    exampleModule.setTrackProducer("pmtrack")
+    exampleModule.setVerbose(True)
+    
+    # Attach an analysis unit ... here we use a base class which do
+    my_proc.add_process(exampleModule)
+
+    my_proc.run(10)
+
+def main():
+
+
+    if len(sys.argv) < 2:
+        print "Error: must include an input file."
+        exit()
+
+    _file = sys.argv[-1]
+    process_file(_file)
+
+
+if __name__ == '__main__':
+  main()
