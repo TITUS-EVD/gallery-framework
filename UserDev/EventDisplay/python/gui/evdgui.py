32de41d5e78d3b40fe012871efebf4c94efdf796
Marco Del Tutto <mdeltutt@fnal.gov>
2020-02-27 09:30:14 -0600

    Beginnning of opflash viewer impl

--- UserDev/EventDisplay/python/gui/evdgui.py
+++ UserDev/EventDisplay/python/gui/evdgui.py
@@ -1,179 +1,6 @@
 from gui import gui
 from pyqtgraph.Qt import QtGui, QtCore
-
-
-class ComboBoxWithKeyConnect(QtGui.QComboBox):
-
-    def __init__(self):
-        super(ComboBoxWithKeyConnect, self).__init__()
-
-    def connectOwnerKPE(self, kpe):
-        self._owner_KPE = kpe
-
-    def keyPressEvent(self, e):
-        if e.key() == QtCore.Qt.Key_Up:
-            super(ComboBoxWithKeyConnect, self).keyPressEvent(e)
-            return
-        if e.key() == QtCore.Qt.Key_Down:
-            super(ComboBoxWithKeyConnect, self).keyPressEvent(e)
-            return
-        else:
-            self._owner_KPE(e)
-
-
-# class CheckableComboBox(QtGui.QComboBox):
-#     def __init__(self):
-#         super(CheckableComboBox, self).__init__()
-#         self.view().pressed.connect(self.handleItemPressed)
-#         self.setModel(QtGui.QStandardItemModel(self))
-
-#     def handleItemPressed(self, index):
-#         item = self.model().itemFromIndex(index)
-#         if item.checkState() == QtCore.Qt.Checked:
-#             item.setCheckState(QtCore.Qt.Unchecked)
-#         else:
-#             item.setCheckState(QtCore.Qt.Checked)
-
-
-class waveformBox(QtGui.QWidget):
-    activated = QtCore.pyqtSignal(str)
-
-    def __init__(self, owner, name, products, default_products=[]):
-        super(waveformBox, self).__init__()
-        # self._label = QtGui.QLabel()
-        self._name = name
-        self._owner = owner
-
-        self._box = QtGui.QToolButton(self)
-        self._box.setMinimumWidth(55)
-        self._box.setText('Select')
-        self._toolmenu = QtGui.QMenu(self)
-        # self.setMinimumWidth(100)
-        self._actions = []
-        for i, product in enumerate(products):
-            action = self._toolmenu.addAction(product.fullName())
-            action.setCheckable(True)
-            if product in default_products:
-                action.setChecked(True)
-            action.triggered.connect(self.emitSignal)
-            self._actions.append(action)
-        self._box.setMenu(self._toolmenu)
-        self._box.setPopupMode(QtGui.QToolButton.InstantPopup)
-        
-        # This is the widget itself, so set it up
-        self._layout = QtGui.QHBoxLayout()
-        # self._layout.addWidget(self._label)
-        # self._layout.addWidget(self._box)
-        self._layout.addWidget(self._box)
-        self.setLayout(self._layout)
-
-
-    def emitSignal(self, text):
-        # self.activated.emit(text)
-        # print('got signal from', self.sender().text(), text)
-        self._active_producers = []
-        for a in self._actions:
-            if a.isChecked():
-                # print ('action', a.text(), 'is checked.')
-                self._active_producers.append(a.text())
-        self._owner.wireChoiceWorker(status=True, activeProducers=self._active_producers)
-        return
-
-    def name(self):
-        return self._name
-
-
-
-
-
-
-
-
-class recoBox(QtGui.QWidget):
-    activated = QtCore.pyqtSignal(str)
-
-    '''
-    A widget class that contains the label and combo box.
-    It also knows what to do when updating
-    '''
-
-    def __init__(self, owner, name, product, producers):
-        super(recoBox, self).__init__()
-        self._label = QtGui.QLabel()
-        self._name = name
-        self._label.setText(self._name.capitalize() + ": ")
-        self._box = ComboBoxWithKeyConnect()
-        self._box.setDuplicatesEnabled(False)
-        self._box.activated[str].connect(self.emitSignal)
-        self._product = product
-        self._producers = producers
-        self._current_producer = None
-        self._stage = "all"
-        if producers == None:
-            self._box.addItem("--None--")
-        else:
-            self._box.addItem("--Select--")
-            for producer in producers:
-                self._box.addItem(producer.producer())
-
-        self._box.connectOwnerKPE(owner.keyPressEvent)
-
-        # This is the widget itself, so set it up
-        self._layout = QtGui.QHBoxLayout()
-        self._layout.addWidget(self._label)
-        self._layout.addWidget(self._box)
-        self.setLayout(self._layout)
-
-    def selectStage(self, stage):
-
-        # If no stage can draw this product, just return
-        if self._producers is None:
-            return
-        else:
-            self._box.clear()
-
-            prod_list = []
-            for prod in self._producers:
-                if prod.stage() == stage or stage == 'all':
-                    prod_list.append(prod.producer())
-
-            if len(prod_list) > 0:
-                self._box.addItem("--Select--")
-                for _producer in prod_list:
-                    self._box.addItem(_producer)
-            else:
-                self._box.addItem("--None--")
-
-        self._box.setDuplicatesEnabled(False)
-
-
-    def keyPressEvent(self, e):
-        self._box.keyPressEvent(e)
-        super(recoBox, self).keyPressEvent(e)
-
-    def emitSignal(self, text):
-        self.activated.emit(text)
-
-    def productType(self):
-        return self._product
-
-    def productObj(self, producer, stage):
-        if stage is None:
-            stage = "all"
-        for prod in self._producers:
-            if prod.producer() == producer:
-                if stage == "all" or prod.stage() == stage:
-                    self._current_producer = producer
-                    return prod
-        self._current_producer = None
-        return None
-
-    def name(self):
-        return self._name
-
-    def currentProducer(self):
-        return self._current_producer
-
+from .boxes import *
 
 
 class evdgui(gui):
@@ -183,11 +10,12 @@ class evdgui(gui):
     override the gui to give the display special features:
     """
 
-    def __init__(self, geometry, manager=None, app=None):
+    def __init__(self, geometry, manager=None, app=None, live=False):
         super(evdgui, self).__init__(geometry)
         if manager is None:
             manager = evd_manager(geometry)
         super(evdgui, self).initManager(manager)
+        self._live = live
         self.initUI()
         self._stage = None
         self._event_manager.fileChanged.connect(self.drawableProductsChanged)
@@ -205,15 +33,15 @@ class evdgui(gui):
     # This function sets up the eastern widget
     def getEastLayout(self):
         # This function just makes a dummy eastern layout to use.
-        label1 = QtGui.QLabel("TITUS")
-        label1a = QtGui.QLabel("The event display")
-        label1b = QtGui.QLabel("for SBN @ Fermilab")
+        self._title1 = QtGui.QLabel("TITUS")
+        self._title1a = QtGui.QLabel("The event display")
+        self._title1b = QtGui.QLabel("for SBN @ Fermilab")
         geoName = self._geometry.name()
-        label2 = QtGui.QLabel('Detector: '+geoName.upper())
-        font = label1.font()
+        self._title2 = QtGui.QLabel('Detector: '+geoName.upper())
+        font = self._title1.font()
         font.setBold(True)
-        label1.setFont(font)
-        label2.setFont(font)
+        self._title1.setFont(font)
+        self._title2.setFont(font)
 
 
 
@@ -221,10 +49,10 @@ class evdgui(gui):
         # This is the total layout
         self._eastLayout = QtGui.QVBoxLayout()
         # add the information sections:
-        self._eastLayout.addWidget(label1)
-        self._eastLayout.addWidget(label1a)
-        self._eastLayout.addWidget(label1b)
-        self._eastLayout.addWidget(label2)
+        self._eastLayout.addWidget(self._title1)
+        self._eastLayout.addWidget(self._title1a)
+        self._eastLayout.addWidget(self._title1b)
+        self._eastLayout.addWidget(self._title2)
         self._eastLayout.addStretch(1)
 
         self._stageLabel = QtGui.QLabel("Stage:")
@@ -407,14 +235,11 @@ class evdgui(gui):
 
     def subtractPedestalWorker(self):
         if self._subtractPedestal.isChecked():
-            print ('_subtractPedestal is checked')
             self._subtract_pedestal = True
         else:
-            print ('_subtractPedestal is NOT checked')
             self._subtract_pedestal = False
 
         if self._view_manager.drawingRawDigits():
-            print ('        calling toggleWires')
             self._event_manager.toggleWires('rawdigit',
                                             stage=self._stage, 
                                             subtract_pedestal=self._subtract_pedestal)
