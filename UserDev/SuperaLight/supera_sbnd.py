f9859d7f6f2cf6852fe58d3f8a35fd4d5ceec8ce
Corey Adams <corey.adams@anl.gov>
2022-02-03 16:58:52 -0600

    readers now load necessary services, but BBox gives a segfault.

--- UserDev/SuperaLight/supera_sbnd.py
+++ UserDev/SuperaLight/supera_sbnd.py
@@ -15,10 +15,15 @@ os.environ['LD_LIBRARY_PATH']=env_val
 
 from ROOT import gallery, galleryfmwk
 from ROOT import supera
+import SBNDservices as services
 
 def process_files(f):
     # a = supera.SuperaModuleBase()
 
+    detClocks       = services.ServiceManager('DetectorClocks')
+    # detProperties   = services.ServiceManager('DetectorProperties')
+    # detPropData     = detProperties.DataFor(detClocks.DataForJob())
+    # geometryCore    = services.ServiceManager('Geometry')
 
     # Create ana_processor instance
     my_proc = galleryfmwk.ana_processor()
@@ -30,8 +35,7 @@ def process_files(f):
 
     out_dir = './'
     out_name = out_dir + base_name.replace(".root", "_larcv.h5")
-
-    supera_light = supera.supera_light()
+    supera_light = supera.supera_light(detClocks.DataForJob())
     supera_light.set_output_file(out_name)
 
     # Attach an analysis unit ... here we use a base class which do
