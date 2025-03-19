689d7b8d45862e15c74eccc60857a7c20abd219f
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2025-03-19 12:50:53 -0500

    This may fix it?

--- UserDev/EventDisplay/python/titus/drawables/__init__.py
+++ UserDev/EventDisplay/python/titus/drawables/__init__.py
@@ -12,7 +12,7 @@ from titus.drawables.mctrack import MCTrack
 from titus.drawables.mctruth import MCTruth
 from titus.drawables.endpoint2d import EndPoint2D
 from titus.drawables.spacepoint import SpacePoint
-from titus.drawables.spacepointhit import SpacePointHit
+from titus.drawables.spacepointhit import SpacepointHits
 from titus.drawables.opflash import OpFlash
 from titus.drawables.febdata import FEBData
 from titus.drawables.opdetwaveform import OpDetWaveform
