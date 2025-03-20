b2626a05153d070dff16367efb2c36d0ec8f3f25
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2025-03-20 13:14:24 -0500

    Trying new idea

--- core/LArUtil/SimpleGeometryHelper.cxx
+++ core/LArUtil/SimpleGeometryHelper.cxx
@@ -86,11 +86,11 @@ Point2D SimpleGeometryHelper::Point_3Dto2D(const TVector3 & _3D_position, unsign
   Double_t planeOrigin[3];
   // geom -> PlaneOriginVtx(plane, planeOrigin);
   // auto vtx = geom.Plane(plane, 0, cryo).GetCenter();
-  auto vtx = geom.PlaneOriginVtx(plane, planeOrigin); //Why center?
+  auto vtx = planeGeo.GetCenter(); //Why center?
   // auto vtx = geom.Plane(plane).GetCenter();
-  planeOrigin[0] = vtx.X();
-  planeOrigin[1] = vtx.Y();
-  planeOrigin[2] = vtx.Z();
+  planeOrigin[0] = 0;//vtx.X();
+  planeOrigin[1] = 0;//vtx.Y();
+  planeOrigin[2] = 0;//vtx.Z();
   // std::cout << "p " << plane << ", t " << tpc << ", c " << cryo << ": plane center x = " << vtx.X() << std::endl;
 
   // Correct for the origin of the planes
