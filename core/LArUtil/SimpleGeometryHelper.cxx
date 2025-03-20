0d283c81cc1432f28548595494822cc5692c5c14
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2025-03-20 13:54:13 -0500

    Adjusting order of shifts

--- core/LArUtil/SimpleGeometryHelper.cxx
+++ core/LArUtil/SimpleGeometryHelper.cxx
@@ -100,6 +100,7 @@ Point2D SimpleGeometryHelper::Point_3Dto2D(const TVector3 & _3D_position, unsign
   // beyond 0 needs to make the time coordinate larger
   // Therefore, subtract the offest (which is already
   // in centimeters)
+  returnPoint.t += trigger_offset(clocks) * fTimeToCm; //cm 
   if (tpc == 0) {
     returnPoint.t = returnPoint.t - planeOrigin[0]; //cm
   } else {
@@ -109,7 +110,6 @@ Point2D SimpleGeometryHelper::Point_3Dto2D(const TVector3 & _3D_position, unsign
 
   // std::cout << "trigger_offset: " << trigger_offset(clocks) << std::endl;
   // std::cout << "fTimeToCm: " << fTimeToCm << std::endl;
-  returnPoint.t += trigger_offset(clocks) * fTimeToCm; //cm 
   // std::cout << "returnPoint.t: " << returnPoint.t << std::endl;
 
   // Set the plane of the Point2D:
