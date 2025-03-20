f3ceebfb0cc22163758f8aee3b7b12291278a036
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2025-03-20 15:56:45 -0500

    Debug

--- UserDev/EventDisplay/RecoViewer/DrawSpacepoint.cxx
+++ UserDev/EventDisplay/RecoViewer/DrawSpacepoint.cxx
@@ -62,7 +62,8 @@ bool DrawSpacepoint::analyze(const gallery::Event & ev) {
         std::cout << " just before Point_3Dto2D call " << std::endl;
         point = geo_helper.Point_3Dto2D(spt.XYZ(), p);
         std::cout << "Just after call" << std::endl;
-        std::cout << " Over here -- About to draw " << point.w << "  " << point.t << " on plane " << point.plane << std::endl;
+        std::cout << " Over here -- About to draw " << point.w << "  " << 
+        point.t << " on plane " << point.plane << " index " <<IndexTracker << " X,Y,Z " << spt.XYZ()[0]  << " , " << spt.XYZ()[1] << " ," <<spt.XYZ()[2]  <<  std::endl;
       }
       catch (const std::exception& e) {
         std::cout << "had an exception" << std::endl;
--- UserDev/EventDisplay/python/titus/drawables/spacepoint.py
+++ UserDev/EventDisplay/python/titus/drawables/spacepoint.py
@@ -97,7 +97,6 @@ class SpacePoint(Drawable):
                     sW = thisPoint.wire() / self._geom.wire2cm()
                     sT = thisPoint.time() / self._geom.time2cm()  #+ self._geom.timeOffsetTicks(thisPlane) 
                     if( TPCCounter == 1 ): 
-                        print("Doing various flips")
                         # Flip the time
                         sT = self._geom.tRange() - sT
                         # Shift up to the appropriate view
