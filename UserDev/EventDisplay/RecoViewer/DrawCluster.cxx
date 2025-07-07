74e57f2f0c73e08d77e85175634ce7e9ac0050f7
root <mclaughlinjacob1996@gmail.com>
2025-07-07 16:15:36 -0500

    Wrong recobase construction

--- UserDev/EventDisplay/RecoViewer/DrawCluster.cxx
+++ UserDev/EventDisplay/RecoViewer/DrawCluster.cxx
@@ -7,8 +7,9 @@ namespace evd {
 
 DrawCluster::DrawCluster(const geo::GeometryCore&               geometry,
                          const detinfo::DetectorPropertiesData& detectorProperties,
+                         const geo::WireReadoutGeom&            wireReadout,
                          const detinfo::DetectorClocksData&     detectorClocks) :
-    RecoBase(geometry, detectorProperties, detectorClocks)
+    RecoBase(geometry, detectorProperties, wireReadout, detectorClocks)
 {
   _name = "DrawCluster";
   _fout = 0;
