d802020b1ea3fb1ca0a1bedc12e9780b04d9b60f
root <mclaughlinjacob1996@gmail.com>
2025-07-07 10:54:34 -0500

    adding types for template:

--- core/LArUtil/LArUtilServicesHandler.h
+++ core/LArUtil/LArUtilServicesHandler.h
@@ -33,6 +33,8 @@
 #include "larcorealg/Geometry/StandaloneGeometrySetup.h"
 #include "larcorealg/Geometry/GeometryCore.h"
 #include "larcorealg/Geometry/WireReadoutGeom.h"
+#include "larcorealg/Geometry/GeoObjectSorterStandard.h"
+#include "larcorealg/Geometry/WireReadoutSorterStandard.h"
 
 // - DetectorClocks
 #include "lardataalg/DetectorInfo/DetectorClocksStandardTestHelpers.h"
